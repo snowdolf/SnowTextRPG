19d5156b42c5feaf55bd726b1bf0c64e0d72a213
snowdolf <snowdolf99@naver.com>
2024-04-25 15:37:27 +0900

    Add death

--- SnowTextRPG/Program.cs
+++ SnowTextRPG/Program.cs
@@ -72,6 +72,12 @@
 
         private void StartScene()
         {
+            if (hp <= 0)
+            {
+                Console.WriteLine("DEATH");
+                return;
+            }
+
             Console.Clear();
             Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
             Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.\n");
